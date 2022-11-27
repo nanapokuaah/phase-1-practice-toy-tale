4ce30712979ede733bf12d95efb0fecda28f0b7a
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-11-27 16:55:40 -0500

    commit tree ca4db9161884ecd6779fe11ff7f530509850db2a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  getToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +14,47 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+toyForm.addEventListener('submit',(e) =>{
+  e.preventDefault();
+  console.log(e.target.image.value, e.target.name.value)
+})
+
+function getToys(){
+  fetch ('http://localhost:3000/toys')
+.then(function(response){
+  return response.json();
+})
+.then(function(data){
+  data.map(t => renderToy(t))
+  console.log(data)
+})
+}
+
+function renderToy(toy){
+  const toyCard = `<div class="card">
+  <h2>${toy.name}</h2>
+  <img src=${toy.image} class="toy-avatar" />
+  <p>${toy.likes}Likes </p>
+  <button class="like-btn">Like <3</button>
+</div>`
+
+const toyBox = document.getElementById('toy-collection')
+toyBox.innerHTML += toyCard
+}
+
+function postToy(name, url)  {
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+headers: {
+
+  "Content-Type": "application/json",
+  Accept: "application/json"
+},
+body: JSON.stringify({
+  "name": "Jessie",
+  "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+  "likes": 0
+})
+})
+}
